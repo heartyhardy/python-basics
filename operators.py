4dd9761e3e724813d6429b78e91eebae117e0103
heartyhardy <charith.n.rathnayake@gmail.com>
2021-10-18 19:59:51 +0530

    Logical and Comparison operators

---
+++ operators.py
@@ -0,0 +1,9 @@
+oranges = 10
+print(f'Number of oranges: {oranges!s}')
+print(f'Number of Oranges is 20? {oranges == 20}' )
+
+apples = 10
+
+print(f'Is same quantity?: {oranges == 10 or apples == 10}')
+print(f'Is apples count equal oranges count?: {oranges == 10 and apples == 20}')
+print(f'Is quantity within range?: {oranges > 5 and apples <= 10}')
