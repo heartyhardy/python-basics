e71d2d0472672334996e0e37792a11b276029c09
heartyhardy <charith.n.rathnayake@gmail.com>
2021-10-19 00:22:47 +0530

    Loops

---
+++ loops.py
@@ -0,0 +1,28 @@
+for i in range(0, 11):
+  print(f'Current iteration is: {i}')
+
+print("\n")
+
+for c in "Hello World!":
+  print(f'Current char: {c}')
+
+print("\n")
+
+languages = ["Rust", "C++", "Python", "Haskell", "JS"]
+for lang in languages:
+  print(lang)
+
+print("\n")
+
+for i in range(0,100, 5):
+  print(f'{i} * 5 = {i*5}')
+
+print("\n")
+
+while languages.__len__() > 0:
+  print(languages.pop())
+else:
+  print(f'\tLanguages array is empty!')
+
+print("\n")
+
